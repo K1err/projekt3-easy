ed5c9034f8ae2c005ce220337b05503c928e3ab7
opilane <opilane@tpt.lan>
2026-04-16 12:45:09 +0300

    js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 const url = "https://tinkr.tech/sdb/poly/wander"
+
+document.getElementById('Saada')
+addEventListener("click", function(){
+
+});
+
+
+
+async function talk(talk) {
+    const messageData = {talk };
+        
+     const response = await fetch('https://tinkr.tech/sdb/poly/wander', {
+        method: 'POST',
+         headers: {
+         'Content-Type': 'application/json'
+         },
+         body: JSON.stringify(messageData)
+        });
+        
+     const result = await response.json();
+    console.log(result);
+    }
+
